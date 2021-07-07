088537c16893b8282907ac8f12d3f07147537f76
tshirishkumar <36203744+tshirishkumar@users.noreply.github.com>
2021-07-07 21:40:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sreeterraformbucket"
+    bucket = "shirishterraformbucket"
     key    = "prod.tfstate"
     region = "us-east-1"
   }
@@ -26,7 +26,7 @@ resource "aws_vpc" "default" {
     enable_dns_hostnames = true
     tags = {
         Name = "${var.vpc_name}"
-	Owner = "Sreeharsha Veerapalli"
+	Owner = "Shirishkumar"
 	environment = "${var.environment}"
     }
 }
