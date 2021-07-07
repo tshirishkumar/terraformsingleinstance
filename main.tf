8b6d903b1b258f2df6010bd671ac0c9674238b14
tshirishkumar <36203744+tshirishkumar@users.noreply.github.com>
2021-07-07 22:26:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -117,8 +117,8 @@ resource "aws_security_group" "allow_all" {
 
 resource "aws_instance" "web-1" {
      ami = var.imagename
-     #ami = "ami-0d857ff0f5fc4e03b"
-     #ami = "${data.aws_ami.my_ami.id}"
+     ami = "ami-0747bdcabd34c712a"
+     ami = "${data.aws_ami.my_ami.id}"
      availability_zone = "us-east-1a"
      instance_type = "t2.micro"
      key_name = "MacKey"
